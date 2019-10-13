42ec1607876ff597f1c208e5bd274bfbabb595b0
Nathan Wong <cloudynathan@gmail.com>
2019-10-12 20:20:32 -0700

    function notes

---
+++ functions.R
@@ -0,0 +1,18 @@
+##### Functions #####
+
+#basic function
+circ.area <- function(x){
+  area <- pi*x^2
+  return(area)
+}
+circ.area(1)
+
+#test function
+a=1000
+
+test <- function(x,y,z){
+  combo <- x+y+z
+  return(combo)
+}
+test(a,2,2)
+
